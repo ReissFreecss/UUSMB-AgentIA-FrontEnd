1470f0b03b70661e542e7c81beb30cd4f3149676
Chr111111s <avilessotelochristian@gmail.com>
2025-06-18 10:37:45 -0600

    chore: remove Dockerfile and nginx configuration files

--- constants.js
+++ constants.js
@@ -1,5 +1,7 @@
 // Global constants for the application
 
-export const API_URL = 'http://localhost:8080';
+// export const API_URL = 'http://localhost:8080';
 
-// export const API_URL = process.env.REACT_APP_API_URL || '/api';
+// En produccion se usa process.env etc, localmente en nuestro puerto 8080 o donde tengas asignado el back
+
+export const API_URL = process.env.REACT_APP_API_URL || '/api';
